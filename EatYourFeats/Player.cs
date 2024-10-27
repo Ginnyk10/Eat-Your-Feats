45d6f5e19c3cd4deb420a41dfef590703e7907eb
Ginny Ke <130810135+Ginnyk10@users.noreply.github.com>
2024-10-27 16:44:19 -0500

    Update Player.cs comments

--- EatYourFeats/Player.cs
+++ EatYourFeats/Player.cs
@@ -5,7 +5,8 @@ Date Revised: 10/27/24
 Purpose: Creates Player class template to store player data 
 */
 
-public class Player
+public class Player // creates player class to store player info in mongodb database for later
+
 {
     public string Id { get; set; } // Unique identifier for the player
     public string Name { get; set; } // Player's name
