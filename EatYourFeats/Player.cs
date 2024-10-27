08c57cbed00a45282ab221a1bb4e8f9251d53c33
Ginny Ke <130810135+Ginnyk10@users.noreply.github.com>
2024-10-27 16:38:22 -0500

    Add files via upload

---
+++ EatYourFeats/Player.cs
@@ -0,0 +1,13 @@
+/*
+Name: Ginny Ke
+Date Created: 10/27/24
+Date Revised: 10/27/24
+Purpose: Creates Player class template to store player data 
+*/
+
+public class Player
+{
+    public string Id { get; set; } // Unique identifier for the player
+    public string Name { get; set; } // Player's name
+    public int Progress { get; set; } // Player's progress in the game
+}
