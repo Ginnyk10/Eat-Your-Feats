669c73a4eea17999c507325db5bde537c28eb12c
jwund <123510044+jwund@users.noreply.github.com>
2024-11-10 22:20:59 -0600

    comment todo.cs

--- EatYourFeats/Models/Todo.cs
+++ EatYourFeats/Models/Todo.cs
@@ -2,10 +2,10 @@
 
 namespace EatYourFeats.Models
 {
-    public class Todo // User model
+    public class Todo // Todo model
     {
-        public ObjectId Id { get; set; }
-        public string Username { get; set; }
+        public ObjectId Id { get; set; } // unique object id
+        public string Username { get; set; } // username depends on which user added the task
         public string Name { get; set; }
         public int Points { get; set; }
         public bool IsCompleted { get; set; }
