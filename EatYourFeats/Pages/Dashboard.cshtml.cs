bc2ae86067162716d7b69e288161b217edbff0b2
dzsailors <dylan.z.sailors@gmail.com>
2024-11-10 21:38:03 -0600

    create new branch for updated code for main

--- EatYourFeats/Pages/Dashboard.cshtml.cs
+++ EatYourFeats/Pages/Dashboard.cshtml.cs
@@ -1,5 +1,14 @@
 ﻿// 4GL for Dashboard page
-using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using EatYourFeats.Services;                // Provides access to UserService for database operations
+using EatYourFeats.Utilities;               // Contains utility methods, such as PasswordHelper for password verification
+using Microsoft.AspNetCore.Authentication;  // Enables authentication functionalities
+using Microsoft.AspNetCore.Authentication.Cookies;  // Defines cookie-based authentication scheme
+using Microsoft.AspNetCore.Mvc;             // Provides attributes and functionality for controllers
+using Microsoft.AspNetCore.Mvc.RazorPages;  // Required for Razor Pages support
+using System.Security.Claims;               // Provides classes for managing and handling user claims
+
 
 namespace EatYourFeats.Pages
 {
@@ -15,6 +24,13 @@ namespace EatYourFeats.Pages
         public void OnGet()
         {
 
+        }
+        //method to log user out and return to splash page
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            //sign user out and remove cookie
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Splash");
         }
     }
 }
