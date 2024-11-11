f039522995bbe510691156614d3db9ca0553aa67
jwund <123510044+jwund@users.noreply.github.com>
2024-11-10 22:24:43 -0600

    comment TodoService.cs

--- EatYourFeats/Services/TodoService.cs
+++ EatYourFeats/Services/TodoService.cs
@@ -1,28 +1,28 @@
-﻿/*
+/*
 Name: Isabel Loney, Jackson Wunderlich
 Date Created: 11/10/2024
 Date Revised: 11/10/2024
-Purpose: 
+Purpose: Allows the Tasks collection in the database to be accessed, so the task list of a user can be retrieved and tasks can be added
 
 Preconditions: MongoDB setup, Tasks table exists, Todo model defined
-Postconditions: 
-Error and exceptions: 
-Side effects: 
-Invariants: 
-Other faults: 
+Postconditions: Tasks retrieved based on provided username, add a task to database
+Error and exceptions: ArgumentNullException can occur if the username is null
+Side effects: N/A
+Invariants: _tasks is always initialized as the Tasks collection in MongoDB
+Other faults: N/A
 */
 
-// Required namespaces for MongoDB functionality and accessing user models
+// Required namespaces for MongoDB functionality and accessing task models
 using MongoDB.Driver;       // MongoDB driver for database interactions
-using EatYourFeats.Models;  // User model
+using EatYourFeats.Models;  // Todo model
 
 namespace EatYourFeats.Services {
-    // Handles database operations for User entities, including retrieval and insertion
+    // Handles database operations for Todo entities, including retrieval and insertion
     public class TodoService {
-        // MongoDB collection that stores User documents
+        // MongoDB collection that stores Todo documents
         private readonly IMongoCollection<Todo> _tasks;
 
-        // Constructor that initializes the collection using a provided MongoDBService instance
+        // Constructor that initializes the tasks collection using a provided MongoDBService instance
         public TodoService(MongoDBService mongoDBService) {
             _tasks = mongoDBService.GetCollection<Todo>("Tasks");
         }
@@ -34,7 +34,6 @@ namespace EatYourFeats.Services {
             return help;
         }
         
-
         // inserts a new task into the database
         public async Task CreateTaskAsync(Todo new_task) =>
             await _tasks.InsertOneAsync(new_task);
